30141c9bf38c9ac4a1541bb334c0dbab91945dcd
Srt1903 <116757092+Srt1903@users.noreply.github.com>
2025-12-29 19:05:01 +0300

    .

--- sort.h
+++ sort.h
@@ -6,12 +6,13 @@ typedef struct {
     int id;
     char title[100];
     char author[100];
-    int isActive;   // 1 = aktif, 0 = lazy delete
+    int isActive;   // 1 = aktif 0 = lazy delete 
 } Book;
 
 
 void quickSortBooks(Book arr[], int low, int high);
 int binarySearchBooks(Book arr[], int size, int id);
+
 
 
 #endif
